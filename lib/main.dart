7f36ff26edf3ccd59c80fe0ad8c107bed57a8068
AlihDrajat <drajat_ts_2@student.smktelkom-sda.sch.id>
2021-08-24 16:06:09 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,60 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Mari Membadut')),
+      drawer: Drawer(
+        elevation: 5,
+        child: Column(
+          children: <Widget>[
+            Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.lightBlue,
+                alignment: Alignment.bottomLeft,
+                child: Text('Badut Menu',
+                    style: TextStyle(color: Colors.white, fontSize: 30))),
+            SizedBox(
+              height: 20,
+            ),
+            ListTile(
+                leading: Icon(
+                  Icons.person,
+                  size: 25,
+                ),
+                title: Text(
+                  'Tentang Kami',
+                  style: TextStyle(fontSize: 25),
+                )),
+            ListTile(
+                leading: Icon(
+                  Icons.question_answer,
+                  size: 25,
+                ),
+                title: Text(
+                  'FAQ',
+                  style: TextStyle(fontSize: 25),
+                )),
+            ListTile(
+                leading: Icon(
+                  Icons.markunread_mailbox,
+                  size: 25,
+                ),
+                title: Text(
+                  'Saran dan Bantuan',
+                  style: TextStyle(fontSize: 25),
+                )),
+            ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  size: 25,
+                ),
+                title: Text(
+                  'Pengaturan',
+                  style: TextStyle(fontSize: 25),
+                ))
+          ],
+        ),
+      ),
       resizeToAvoidBottomInset: false,
       body: Container(
         height: MediaQuery.of(context).size.width,
