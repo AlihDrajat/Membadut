affc41c20aae0db539f2152fc992ee4823d35afc
AlihDrajat <drajat_ts_2@student.smktelkom-sda.sch.id>
2021-09-03 17:09:41 +0700

    initial commit

---
+++ lib/drawer.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:login_cenah/main.dart';
+import 'package:login_cenah/pengaturan.dart';
+import 'package:login_cenah/pertanyaan.dart';
+import 'package:login_cenah/saran.dart';
+import 'package:login_cenah/tentang.dart';
+
+class Drawers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          _drawerHeader(),
+          _drawerItem(
+            icon: Icons.person,
+            text: 'Tentang Kami',
+            onTap: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return AboutUs();
+              }));
+            },
+          ),
+          _drawerItem(
+            icon: Icons.message,
+            text: 'Saran dan Bantuan',
+            onTap: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return Suggest();
+              }));
+            },
+          ),
+          _drawerItem(
+            icon: Icons.question_answer,
+            text: 'FAQ',
+            onTap: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return FAQ();
+              }));
+            },
+          ),
+          _drawerItem(
+            icon: Icons.settings,
+            text: 'Pengaturan',
+            onTap: () => {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return Setting();
+              })),
+            },
+          ),
+          Divider(height: 25, thickness: 1),
+          _drawerItem(
+            icon: Icons.logout,
+            text: 'Logout',
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return MyApp();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _drawerHeader() {
+  return UserAccountsDrawerHeader(
+    currentAccountPicture: ClipOval(
+      child: Image(image: AssetImage('assets/istri.jpg'), fit: BoxFit.cover),
+    ),
+    otherAccountsPictures: [
+      ClipOval(
+        child:
+            Image(image: AssetImage('assets/sddefault.jpg'), fit: BoxFit.cover),
+      ),
+      ClipOval(
+        child:
+            Image(image: AssetImage('assets/DSCF6181.jpg'), fit: BoxFit.cover),
+      )
+    ],
+    accountName: Text('CiwiG0'),
+    accountEmail: Text('blablalba@gmail.com'),
+  );
+}
+
+Widget _drawerItem({IconData? icon, String? text, GestureTapCallback? onTap}) {
+  return ListTile(
+    title: Row(
+      children: <Widget>[
+        Icon(icon),
+        Padding(
+          padding: EdgeInsets.only(left: 25.0),
+          child: Text(
+            text!,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+    onTap: onTap,
+  );
+}
--- lib/rumah.dart
+++ lib/rumah.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_cenah/main.dart';
-import 'package:login_cenah/pengaturan.dart';
-import 'package:login_cenah/pertanyaan.dart';
-import 'package:login_cenah/saran.dart';
-import 'package:login_cenah/tentang.dart';
+import 'drawer.dart';
 
 class Rumah extends StatefulWidget {
   @override
@@ -17,91 +14,8 @@ class _RumahState extends State<Rumah> {
         appBar: AppBar(
           title: Text('Ini Halaman Utama'),
         ),
-        drawer: Drawer(
-          elevation: 5,
-          child: Column(
-            children: <Widget>[
-              Container(
-                  width: double.infinity,
-                  height: 100,
-                  color: Colors.lightBlue,
-                  alignment: Alignment.bottomLeft,
-                  child: Text('Badut Menu',
-                      style: TextStyle(color: Colors.white, fontSize: 30))),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) {
-                    return AboutUs();
-                  }));
-                },
-                child: ListTile(
-                    leading: Icon(
-                      Icons.person,
-                      size: 25,
-                    ),
-                    title: Text(
-                      'Tentang Kami',
-                      style: TextStyle(fontSize: 25),
-                    )),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) {
-                    return FAQ();
-                  }));
-                },
-                child: ListTile(
-                    leading: Icon(
-                      Icons.question_answer,
-                      size: 25,
-                    ),
-                    title: Text(
-                      'FAQ',
-                      style: TextStyle(fontSize: 25),
-                    )),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) {
-                    return Suggest();
-                  }));
-                },
-                child: ListTile(
-                    leading: Icon(
-                      Icons.markunread_mailbox,
-                      size: 25,
-                    ),
-                    title: Text(
-                      'Saran dan Bantuan',
-                      style: TextStyle(fontSize: 25),
-                    )),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) {
-                    return Setting();
-                  }));
-                },
-                child: ListTile(
-                    leading: Icon(
-                      Icons.settings,
-                      size: 25,
-                    ),
-                    title: Text(
-                      'Pengaturan',
-                      style: TextStyle(fontSize: 25),
-                    )),
-              )
-            ],
-          ),
-        ),
+        drawer: Drawers(),
+        endDrawer: Drawers(),
         body: Center(
             child: Column(
           children: <Widget>[
