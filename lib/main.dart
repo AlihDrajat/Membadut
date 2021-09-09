e012f8315f02d4c0114b61f7f0e05a0f0e30c29f
AlihDrajat <drajat_ts_2@student.smktelkom-sda.sch.id>
2021-09-09 13:38:50 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,18 @@ import 'package:flutter/material.dart';
 import 'package:login_cenah/rumah.dart';
 
 final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+// Diluc
+  'https://images.alphacoders.com/110/1109227.jpg',
+// Lisa
+  'https://images4.alphacoders.com/110/1107815.jpg',
+// Zhongli Lantern Rite
+  'https://images6.alphacoders.com/113/1132140.png',
+// Windrise
+  'https://images3.alphacoders.com/112/1123012.jpg',
+// Fish Blasting
+  'https://images8.alphacoders.com/110/1109289.jpg',
+  // Fatui
+  'https://images7.alphacoders.com/113/1132533.jpg'
 ];
 
 void main() => runApp(MyApp());
@@ -36,7 +42,7 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   int _currentIndex = 0;
-  List<int> cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> cardList = [1, 2, 3, 4, 5, 6];
 
   List<T> map<T>(List list, Function handler) {
     List<T> result = [];
