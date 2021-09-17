1c36cf0976b0d8a456281389cfb2b30d5452e52a
AlihDrajat <drajat_ts_2@student.smktelkom-sda.sch.id>
2021-09-17 16:55:49 +0700

    initial commit

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -14,20 +14,85 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.user.name,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_horiz),
-            onPressed: () {},
+        backgroundColor: Color.fromARGB(255, 156, 92, 54),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(200, 156, 92, 54),
+          elevation: 1,
+          title: Text(
+            'Direct Message',
+            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
           ),
-        ],
-      ),
-      body: Text('UwU'),
-    );
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(
+                  top: 3,
+                  bottom: 5,
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 37,
+                          backgroundImage: AssetImage(widget.user.imageUrl),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.user.name,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.58,
+                              child: Text(
+                                widget.user.lastSeen,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 12,
+                                    color: Colors.white),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.more_horiz,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 88, 92, 95),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
